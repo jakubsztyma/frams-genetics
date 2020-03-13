187912e32635df71c1912d4ff38149018057d736
Jakub Sztyma <jakubsztyma@interia.pl>
2020-03-13 17:57:49 +0100

    Separate fh_solid test

--- tests/go.sh
+++ tests/go.sh
@@ -2,7 +2,7 @@
 
 # "$@" allows to pass any command-line arguments of this script to python tester
 
-for testset in SDK-tests.txt geometry-tests.txt similarity-tests.txt
+for testset in SDK-tests.txt geometry-tests.txt similarity-tests.txt similarity-solid-tests.txt
 do
 	python3 ../../tester/tester.py "$@" -e '(.*)=../\1' -f $testset 
 done
